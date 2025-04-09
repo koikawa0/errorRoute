5b3569e8b1167c879497b18062340fe7a4f94254
Koikawa <kaiofittipaldisc@gmail.com>
2025-04-09 19:13:48 -0300

    fixed

--- src/Pages/Postagem.jsx
+++ src/Pages/Postagem.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom"
+import Titulo from "../Components/Titulo"
+import Conteudo from "../Components/Conteudo"
+import Campo from "../Components/Campo"
 
 export default function Postagem() {
   const { nome, descricao } = useParams()
+  console.log("Parametros foram recebidos", nome, descricao)
   return <>
   <Titulo nome="Mergulhando no React"/>
   <Campo>
